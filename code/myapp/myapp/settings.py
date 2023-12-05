c40514b16ce2f49d602e13bc43d5731dd9868dec
DzunCcat <sanokut0914@gmail.com>
2023-12-05 15:41:05 +0900

    rebuild project fixed runserver python3 manage.py runserver 0.0.0.0:8000

--- code/myapp/myapp/settings.py
+++ code/myapp/myapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)irgu@_vti0+!br%c#$_0g@$93@lf#q%zwn*h%z$m3j=)5w@yy'
+SECRET_KEY = 'django-insecure-yngghk&f4z56hg$8fdvq7kk0&nv&c&4kara--3#(3a!9liw^1_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'memorymap.apps.MemorymapConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Memorymap',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
 
 
 # Password validation
