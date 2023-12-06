cbbe2e899b10ab337e4955290d18684064216850
DzunCcat <sanokut0914@gmail.com>
2023-12-06 15:41:25 +0900

    DjangoAdminSite add. article_list(test.html) add.

--- code/myapp/memorymap/models.py
+++ code/myapp/memorymap/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=2000)
+    image = models.ImageField(upload_to='article_images/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+    )
+    
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
