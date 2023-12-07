6db1cf9b8647734d476fb277c8ccd6c42f1d99d0
DzunCcat <sanokut0914@gmail.com>
2023-12-07 15:55:46 +0900

    CMD[docker compose build] templates/article_list <img> fixed -setting.py fixed -myapp/urls.py fixed templates/base.html add -article_list apply

--- code/myapp/myapp/urls.py
+++ code/myapp/myapp/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('memorymap/', include('memorymap.urls')),
     path('admin/', admin.site.urls),
     path('',  RedirectView.as_view(url='/memorymap/')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
