8819d358848561e85fe9c27e0ae57b8c3669c465
DzunCcat <sanokut0914@gmail.com>
2023-12-10 08:55:22 +0900

    Update,Create,Delete add change migrate bug:Google.api define

--- code/myapp/memorymap/views.py
+++ code/myapp/memorymap/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import Category, Article
 
-# Create your views here.
+class IndexView(generic.ListView):
+    model = Article
+
+class DetailView(generic.DetailView):
+    model = Article
+
+class CreateView(generic.edit.CreateView):
+    model = Article
+    fields = '__all__'
+
+class UpdateView(generic.edit.UpdateView):
+    model = Article
+    fields = '__all__'
+
+class DeleteView(generic.edit.DeleteView):
+    model = Article
+    success_url = reverse_lazy('memorymap:index')
--- code/myapp/myapp/settings.py
+++ code/myapp/myapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Memorymap',
         'USER': 'postgres',
         'PASSWORD': 'password',
@@ -109,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Media setting
+MEDIA_URL = ''
+MEDIA_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
